3291ddc9e9d2547f32a9adcbba0c95fea0f17d25
lollipop-onl <lollipop@simochee.net>
2020-12-23 23:18:23 +0900

    ✨ Setup Vite and tailwindcss

---
+++ workspaces/www/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-red-500">hello, you.</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+});
+</script>
---
+++ workspaces/www/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    safeList: [],
+    content: [
+      './index.html',
+      './src/**/*.{vue,js}',
+    ],
+  },
+};
